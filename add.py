c80f89fb4084f1516622bb302c0f7caa02c796e6
Tal Shafir <tal.4000@gmail.com>
2020-06-28 15:12:46 +0300

    fixed code to have perfect pylint score

--- add.py
+++ add.py
@@ -1,2 +1,13 @@
+"""
+Simple adding module
+"""
+
+
 def add(number1: int, number2: int) -> int:
+    """
+    Get two numbers and return their sum
+    :param number1:
+    :param number2:
+    :return: sum of the two numbers
+    """
     return number1 + number2
--- test_add.py
+++ test_add.py
@@ -1,10 +1,20 @@
+"""
+Unit test for add module
+"""
+
 import unittest
 
 from add import add
 
 
-class MyTestCase(unittest.TestCase):
+class AddTestCase(unittest.TestCase):
+    """
+        Test case for add function
+    """
     def test_add(self):
+        """
+        Check a simple addition with add function
+        """
         self.assertEqual(add(2, 2), 4)
 
 
